7bad58cd1038e577b2bc0e422a76bb18407d3d0e
That Binh <tonthatbinh135@gmail.com>
2017-08-01 21:33:39 +0700

    add new comments to the ngIf directives

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,7 +1,10 @@
 import {Component, Input} from '@angular/core';
 import { Hero } from './hero';
+//extract the Master/detail component
 @Component({
     selector:'hero-detail',
+    //ngIf directive conditionally adds or removes content from 
+    //the DOM based on whether or not an expression is true or false
     template:`
     <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
