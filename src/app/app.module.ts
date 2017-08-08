3cf8b305a559dc60187b1961dea2a03d6eea78c8
That Binh <tonthatbinh135@gmail.com>
2017-08-08 09:49:53 +0700

    mock up get data from web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
-import { HeroService }          from './hero.service';
+import { HeroService } from './hero.service';
 //import { RouterModule } from '@angular/router';
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    // simulates communication with the remote server by adding the InMemoryWebApiModule to the module imports, 
+    //effectively replacing the Http client's XHR backend service with an in-memory alternative.
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+    
+    ],
+  declarations: [
+    AppComponent, 
+    HeroDetailComponent, 
+    HeroesComponent, 
+    DashboardComponent],
   providers: [HeroService],
   bootstrap: [AppComponent]
 })
