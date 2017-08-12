1e8b5842cd9ea7b0c4aa5d3c689ac17077d317ee
That Binh <tonthatbinh135@gmail.com>
2017-08-12 14:50:22 +0700

    Minor changes in package.json

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -19,6 +19,6 @@ export class DashboardComponent implements OnInit {
     //Call the service on the ngOnInit lifecycle
     ngOnInit(): void {
        this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
+      .then(heroes => this.heroes = heroes.slice(3, 7));
     }
 }
